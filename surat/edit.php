ef2f09d55e8068cf96de21afc29f7f71d72e18c3
idiotRun <doflamingod642@gmail.com>
2023-09-13 10:00:12 +0800

    improve surat css

--- surat/edit.php
+++ surat/edit.php
@@ -32,7 +32,7 @@ $result= mysqli_fetch_assoc($data);
                
                 <div class= "form-group">
                     <label for="tanggal_surat">tanggal_surat</label>
-                    <input type="text" name="tanggal_surat" class="form-control" value="<?php echo $result['tanggal_surat']?>">
+                    <input type="date" name="tanggal_surat" class="form-control" value="<?php echo $result['tanggal_surat']?>">
                 </div>
                 
                 <div class= "form-group">
@@ -52,7 +52,7 @@ $result= mysqli_fetch_assoc($data);
                 
                 <div class= "form-group">
                     <label for="tanggal_agenda">Tanggal Agenda</label>
-                    <input type="text" name="tanggal_agenda" class="form-control" value="<?php echo $result['tanggal_agenda']?>">
+                    <input type="date" name="tanggal_agenda" class="form-control" value="<?php echo $result['tanggal_agenda']?>">
                 </div>
                 
                 <div class= "form-group">
--- surat/tambah.php
+++ surat/tambah.php
@@ -2,86 +2,90 @@
 
 <?php require_once('../template/menu.php') ?>
 
-<div class="card">
-    <div class="card-body">
-        <div class="row">
-            <form action="proses_tambah.php" method="post">
+<div class="container-fluid">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <form action="proses_tambah.php" method="post">
 
-                    <!-- nomor surat -->
-                    <div class="form-group">
-                        <label for="exampleFormControlInput1">Nomor Surat</label>
-                        <input type="text" name="nomor_surat" class="form-control" id="exampleFormControlInput1" placeholder="Nomor Surat">
-                    </div>
-
-                    
-                    <!-- tanggal surat -->
-                    <div class="form-group">
-                        <label for=""> Tanggal Surat :</label>
-                        <input type="date" name="tanggal_surat" class="form-control">
-                    </div>
-
-                    <!-- pengirim surat -->
-                    <div class="form-group">
-                        <label for=""> Pengirim :</label>
-                        <input type="text" name="pengirim" class="form-control">
-                    </div>
-
-                    <!-- penerima surat -->
-                    <div class="form-group">
-                    
-                        <label for=""> Penerima :</label>
-                        <input type="text" name="penerima" class="form-control">
-                    </div>
-
-                    <!-- nomor agenda -->
-                    <div class="form-group">
-                        <label for=""> Nomor Agenda :</label>
-                        <input type="text" name="nomor_agenda" class="form-control">
-                    </div>
-
-                    <!-- tanggal agenda -->
-                    <div class="form-group">
-                        <label for=""> Tanggal Agenda :</label>
-                        <input type="date" name="tanggal_agenda" class="form-control">
-                    </div>
-
-                    <!-- option buku -->
-                    <select name="buku" class="custom-select custom-select-lg mb-3" >
-                        <option value="">--pilih buku--</option>
-                        
-                        <!-- pengulangan untuk opsi pemilihan buku -->
-                        <?php
-                        $datax = "SELECT * FROM buku";
-                        $pilih = mysqli_query($conn, $datax);
-
-                        foreach ($pilih as $key ) :?>
-                        
-                        <option value="<?= $key['id'] ?>"> <?= $key['nama'] ?> </option>
-
-                        <!-- akhir pengulangan -->
-                        <?php endforeach ?>
-                    </select>
+                        <!-- nomor surat -->
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Nomor Surat</label>
+                            <input type="text" name="nomor_surat" class="form-control" id="exampleFormControlInput1" placeholder="Nomor Surat">
+                        </div>
 
 
-                    <!-- pilihan status  -->
-                    <select name="status" class="custom-select custom-select-lg mb-3">
-                        <option value=""> --pilih status-- </option>
-                        <option value="proses"> proses </option>
-                        <option value="selesai"> selesai </option>
-                        <option value="tunda"> tunda </option>
-                    </select>
+                        <!-- tanggal surat -->
+                        <div class="form-group">
+                            <label for=""> Tanggal Surat :</label>
+                            <input type="date" name="tanggal_surat" class="form-control">
+                        </div>
 
-                    <!-- pili tipe surat -->
-                    <select name="tipe_surat" class="custom-select custom-select-lg mb-3">
-                        <option value=""> --pilih tipe surat-- </option>
-                        <option value="masuk"> masuk </option>
-                        <option value="keluar"> keluar </option>
-                    </select>
+                        <!-- pengirim surat -->
+                        <div class="form-group">
+                            <label for=""> Pengirim :</label>
+                            <input type="text" name="pengirim" class="form-control">
+                        </div>
+
+                        <!-- penerima surat -->
+                        <div class="form-group">
+
+                            <label for=""> Penerima :</label>
+                            <input type="text" name="penerima" class="form-control">
+                        </div>
+
+                        <!-- nomor agenda -->
+                        <div class="form-group">
+                            <label for=""> Nomor Agenda :</label>
+                            <input type="text" name="nomor_agenda" class="form-control">
+                        </div>
+
+                        <!-- tanggal agenda -->
+                        <div class="form-group">
+                            <label for=""> Tanggal Agenda :</label>
+                            <input type="date" name="tanggal_agenda" class="form-control">
+                        </div>
+
+                        <!-- option buku -->
+                        <select name="buku" class="custom-select custom-select-lg mb-3">
+                            <option value="">--pilih buku--</option>
+
+                            <!-- pengulangan untuk opsi pemilihan buku -->
+                            <?php
+                            $datax = "SELECT * FROM buku";
+                            $pilih = mysqli_query($conn, $datax);
+
+                            foreach ($pilih as $key) : ?>
+
+                                <option value="<?= $key['id'] ?>"> <?= $key['nama'] ?> </option>
+
+                                <!-- akhir pengulangan -->
+                            <?php endforeach ?>
+                        </select>
+
+
+                        <!-- pilihan status  -->
+                        <select name="status" class="custom-select custom-select-lg mb-3">
+                            <option value=""> --pilih status-- </option>
+                            <option value="proses"> proses </option>
+                            <option value="selesai"> selesai </option>
+                            <option value="tunda"> tunda </option>
+                        </select>
+
+                        <!-- pili tipe surat -->
+                        <select name="tipe_surat" class="custom-select custom-select-lg mb-3">
+                            <option value=""> --pilih tipe surat-- </option>
+                            <option value="masuk"> masuk </option>
+                            <option value="keluar"> keluar </option>
+                        </select>
 
                         <!-- tombol tambah data -->
-                    <button type="submit" name="tambah" class="btn btn-primary"> Tambah Data </button>
+                        <button type="submit" name="tambah" class="btn btn-primary"> Tambah Data </button>
 
-            </form>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -94,4 +98,5 @@
 
 
 </body>
+
 </html>
