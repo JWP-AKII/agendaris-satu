5e0f016352a73fc52ffeb6845a47e81eb64e1472
idiotRun <doflamingod642@gmail.com>
2023-09-13 14:06:19 +0800

    fix bugs

--- surat/proses_tambah.php
+++ surat/proses_tambah.php
@@ -11,19 +11,20 @@ if (isset($_POST['tambah'])) {
     $buku = $_POST['buku'];
     $status = $_POST['status'];
     $tipe_surat = $_POST['tipe_surat'];
+    $date = date('Y-m-d H:i:s');
 
 
 
     $sql = "INSERT INTO surat VALUES
-            (NULL, '$nomor_surat', '$tanggal_surat', '$pengirim', '$penerima', '$nomor_agenda', '$tanggal_agenda', '$buku', '$status', '$tipe_surat', NULL)";
+            (NULL, '$nomor_surat', '$tanggal_surat', '$pengirim', '$penerima', '$nomor_agenda', '$tanggal_agenda', '$buku', '$status', '$tipe_surat', '$date')";
 
     $result = mysqli_query($conn, $sql);
     
     if ($result) {
-        echo "<script>alert('Data Berhasil diubah :p'); window.location.href = 'index.php';</script>";
+        echo "<script>alert('Data Berhasil ditambah :p'); window.location.href = 'index.php';</script>";
         exit;
     } else{
-        echo "data gagal di tambahkan";
+        echo mysqli_error($conn);
     }
 }
 
