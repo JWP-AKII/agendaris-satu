bd09569285bc66fcbf3f1e71afe773ea44da0438
Luthfihakimah <luthfi44427@gmail.com>
2023-09-13 10:30:42 +0800

    reza : crud user

--- user/proses_tambah.php
+++ user/proses_tambah.php
@@ -1,7 +1,13 @@
  <?php
  
- include '../conn.php';
+    include '../conn.php';
 
- 
+    $username =$_POST['username'];
+    $password =$_POST['password'];
+    $jabatan =$_POST['jabatan'];
+
+    mysqli_query($conn, "insert into user values('', '$username', '$password', '$jabatan', '')");
+
+    header('location:index.php');
  
  ?>
