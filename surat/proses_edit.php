4ae070ccf06ce70ec2bc544dfd28729db15e2761
Deswynta Abelia <144751930+Deswhineta@users.noreply.github.com>
2023-09-12 17:52:23 -0700

    perbaikan proses_edit.php bagian $sql
    salah penempatan koma

--- surat/proses_edit.php
+++ surat/proses_edit.php
@@ -16,7 +16,7 @@ if (isset($_POST['simpan'])) {
     $tisurat= $_POST['tipe_surat'];
 
     $sql= "UPDATE surat 
-            SET nomor_surat='$nosurat,'
+            SET nomor_surat='$nosurat',
             tanggal_surat='$tasurat',
             pengirim='$pengirim',
             penerima='$penerima',
